2a8cf5c669afe6c52671669cd91a53bd07408d59
aakashenbake <aakash.enbake@gmail.com>
2015-08-28 16:25:39 +0530

    reduced code

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -53,7 +53,6 @@ class AlbumsController < ApplicationController
 
 private
   def album_call
-    debugger
     @album ||= params[:id].present? ? Album.find(params[:id]) : params[:action]=="create" ? current_user.albums.new(album_params): Album.new
     @picture ||= Picture.where(:album_id => params[:id]).order(:name)
   end
